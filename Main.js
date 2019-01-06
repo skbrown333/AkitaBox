1c50a8624f6078d83682dcff13ae36d72ff04e27
Steffan Brown <steffan@Steffans-MacBook-Pro.local>
2019-01-06 15:17:34 -0600

    fix(Hand): remove test data

--- Main.js
+++ Main.js
@@ -1,27 +1,26 @@
 main();
 function main() {
-	
-	// Initialize an empty Deck and and Empty Hand.
-	var deck = new Deck();
-	var hand = new Hand();
-	// Load cards into the Deck.
-	deck.loadDeck();
-	deck.shuffleCards();
+  // Initialize an empty Deck and and Empty Hand.
+  var deck = new Deck();
+  var hand = new Hand();
+  // Load cards into the Deck.
+  deck.loadDeck();
+  deck.shuffleCards();
 
-	// Draw 5 cards into Hand.
-	hand.addCard(deck.popCard());
-	hand.addCard(deck.popCard());
-	hand.addCard(deck.popCard());
-	hand.addCard(deck.popCard());
-	hand.addCard(deck.popCard());
+  // Draw 5 cards into Hand.
+  hand.addCard(deck.popCard());
+  hand.addCard(deck.popCard());
+  hand.addCard(deck.popCard());
+  hand.addCard(deck.popCard());
+  hand.addCard(deck.popCard());
 
-	// Display Hand on screen.
-	for(var i = 0; i < hand.cards.length; i++) {
-		document.getElementById(i+1).src = "public/images/" + hand.cards[i].suit + "_" + hand.cards[i].rank + ".jpg";
-	}
+  // Display Hand on screen.
+  for (var i = 0; i < hand.cards.length; i++) {
+    document.getElementById(i + 1).src =
+      "public/images/" + hand.cards[i].suit + "_" + hand.cards[i].rank + ".jpg";
+  }
 
-	document.getElementById("high").innerHTML = "High Card: " + hand.high.rank;
-	document.getElementById("score").innerHTML = "Score   : " + hand.score().string;
-	
+  document.getElementById("high").innerHTML = "High Card: " + hand.high.rank;
+  document.getElementById("score").innerHTML =
+    "Score   : " + hand.score().string;
 }
-
